3af84e5c9957c3fb744e198fd926d2c6b145908f
Ece Polat <ece_polat1409@hotmail.com>
2026-02-22 19:48:09 +0300

    add unit test

--- src/test/java/com/ecepolat/exception_management/ExceptionManagementApplicationTests.java
+++ src/test/java/com/ecepolat/exception_management/ExceptionManagementApplicationTests.java
@@ -1,13 +1,41 @@
 package com.ecepolat.exception_management;
 
+import com.ecepolat.dto.DtoEmployee;
+import com.ecepolat.service.IEmployeeService;
+import com.ecepolat.starter.ExceptionManagementStarter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {ExceptionManagementStarter.class})
 class ExceptionManagementApplicationTests {
 
+	@Autowired
+	private IEmployeeService employeeService;
+
+	@BeforeEach
+	public void beforeEach(){
+		System.out.println("beforeEach çalıştı...");
+	}
+
 	@Test
-	void contextLoads() {
+	public void testFindEmployeeById() {
+		DtoEmployee employee = employeeService.findEmployeeById(2L);
+
+		//assertNotNull(employee);
+
+		if (employee!=null){
+			System.out.println("isim : " + employee.getName());
+		}
+	}
+
+	@AfterEach
+	public void afterEach(){
+		System.out.println("AfterEach çalıştı.");
 	}
 
 }
